075445ebd1b64a6a9bdfb41580b1f6754b7b6ade
NarendraVB <vbnarendra4@gmail.com>
2026-08-06 19:58:12 +0530

    feat(realtime): enable automatic dashboard polling

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import Providers from "./app/providers";
 import App from "./app/App";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Providers>
+      <App />
+    </Providers>
   </React.StrictMode>
 );
